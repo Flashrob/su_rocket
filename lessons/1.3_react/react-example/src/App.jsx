53f93150143159a59ccbfeaaa308b20eb220b3af
Flashrob <kolsek.income@gmail.com>
2024-10-28 20:46:48 +0800

    Controlled form example

--- lessons/1.3_react/react-example/src/App.jsx
+++ lessons/1.3_react/react-example/src/App.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [submittedTitle, setSubmittedTitle] = useState("");
   return (
     <>
       <h1>React App</h1>
+
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setSubmittedTitle(value);
+          setValue("");
+        }}
+      >
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setSubmittedTitle(value);
+          setValue("");
+        }}
+      >
+        Submit
+      </button>
+      {submittedTitle && <h1>{submittedTitle}</h1>}
     </>
   );
 }

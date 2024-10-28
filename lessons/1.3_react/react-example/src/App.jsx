389ef6245f7eba39eda7eeeb96b055d24ae8946f
Flashrob <kolsek.income@gmail.com>
2024-10-28 14:05:14 +0800

    Add more conditional rendering

--- lessons/1.3_react/react-example/src/App.jsx
+++ lessons/1.3_react/react-example/src/App.jsx
@@ -4,14 +4,23 @@ import { Secret } from "./components/Secret";
 
 function App() {
   const [showSecret, setShowSecret] = useState(false);
+  const [yourSecret, setYourSecret] = useState("");
+
   return (
     <>
       <h1>React App</h1>
       {/* {showSecret ? <Secret /> : null} */}
       {showSecret && <Secret />}
       <button onClick={() => setShowSecret(!showSecret)}>
-        Click me for a big secret
+        {showSecret ? "Click me to hide secret" : "Click me for a big secret"}
       </button>
+      {showSecret && (
+        <input
+          onChange={(e) => setYourSecret(e.target.value)}
+          placeholder="Now tell me your secret"
+        />
+      )}
+      {yourSecret && showSecret && <p>Your secret: {yourSecret}</p>}
     </>
   );
 }
